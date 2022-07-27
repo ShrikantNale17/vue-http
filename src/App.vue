c9e3c6bf46e808f10b1a2baff13677fb0787b2bb
ShrikantNale-angularminds <shrikant.nale@angularminds.in>
2022-07-27 10:50:02 +0530

    HTTP requests

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CreatePost />
+  <PostList />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PostList from "./components/PostList.vue";
+import CreatePost from "./components/CreatePost.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    PostList,
+    CreatePost,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/CreatePost.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <form @submit.prevent="createPost">
+      <div>
+        <label for="title">Title : </label>
+        <input type="text" id="title" v-model="formData.title" />
+      </div>
+      <div>
+        <label for="body">Body : </label>
+        <input type="text" id="body" v-model="formData.body" />
+      </div>
+      <div>
+        <label for="userId">UserId : </label>
+        <input type="text" id="userId" v-model="formData.userId" />
+      </div>
+      <button>Add Post</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "CreatePost",
+  data() {
+    return {
+      formData: {
+        title: "",
+        body: "",
+        userId: null,
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      axios
+        .post(`https://jsonplaceholder.typicode.com/posts`, this.formData)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
+
+<style scoped></style>
